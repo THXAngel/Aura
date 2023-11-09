44746d540b3956bfbf8ff7dabc24a4c71b400371
THXAngel <124478916+THXAngel@users.noreply.github.com>
2023-11-09 08:38:26 -0500

    29

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Glitter
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
